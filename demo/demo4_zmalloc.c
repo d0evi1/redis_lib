2d0cbe2ecf0f6444c5aff68bc67bc2b32cee5db6
d0evi1 <3331000@qq.com>
2014-11-17 22:24:35 +0800

    update zmalloc demo.

--- demo/demo4_zmalloc.c
+++ demo/demo4_zmalloc.c
@@ -7,12 +7,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+void redisPanic(char* msg, char* file, int line) 
+{
+    printf("bug: %s #%s:%d\n", msg, file, line);
+    *((char*)-1) = 'x';
+}
+    
+
+
+void redisOutOfMemoryHandler(size_t allocation_size)
+{
+    printf("Out Of Memory allocating %zu bytes!\n"), allocation_size;
+    redisPanic("redis out of memory", __FILE__, __LINE__);
+}
+
+
 int main()
 {
+    // step 1: init (optional).
+    zmalloc_enable_thread_safeness();
+    zmalloc_set_oom_handler(redisOutOfMemoryHandler);
+
+    // step 2: malloc.
     char* pMem = zmalloc(sizeof(char)*100);
     strcpy(pMem, "hello world.");
     printf("str=%s\n", pMem);
-    printf("size=%d\n", zmalloc_size(pMem));
+    printf("size=%zu\n", zmalloc_size(pMem));
     zlibc_free(pMem);
 
     return 0;
